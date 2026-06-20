4ed4f016ca776fb7447874572e2d487f6b3c0da6
Arky <20231736@nbsc.edu.ph>
2026-06-20 15:27:06 +0800

    created Login.tsx

---
+++ src/pages/Login.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { IonPage, IonContent, IonInput, IonButton, IonTitle } from '@ionic/react';
+import { supabase } from '../services/supabase';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) return alert(error.message);
+
+    window.location.href = '/dashboard';
+  };
+
+  return (
+    <IonPage>
+      <IonContent className="ion-padding">
+
+        <IonTitle>Admin Login</IonTitle>
+
+        <IonInput placeholder="Email" onIonChange={e => setEmail(e.detail.value!)} />
+        <IonInput type="password" onIonChange={e => setPassword(e.detail.value!)} />
+
+        <IonButton expand="block" onClick={login}>
+          Login
+        </IonButton>
+
+      </IonContent>
+    </IonPage>
+  );
+}
