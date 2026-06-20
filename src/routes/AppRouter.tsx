f5f707b02325fedf2055d70074efa508a880225f
Arky <20231736@nbsc.edu.ph>
2026-06-20 15:25:25 +0800

    Added supabase service and added approuter

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,18 @@
+import { Route, Redirect } from 'react-router-dom';
+import Setup from '../pages/Setup';
+import Login from '../pages/Login';
+import Dashboard from '../pages/Dashboard';
+
+export default function AppRouter() {
+  return (
+    <>
+      <Route exact path="/">
+        <Redirect to="/setup" />
+      </Route>
+
+      <Route path="/setup" component={Setup} />
+      <Route path="/login" component={Login} />
+      <Route path="/dashboard" component={Dashboard} />
+    </>
+  );
+}
