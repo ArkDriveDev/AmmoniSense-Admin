5def6d4c26c3937d71fd3031798649b95b30c214
Arky <20231736@nbsc.edu.ph>
2026-06-21 09:44:58 +0800

    Updated Approuter to call protectedroute

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,7 +1,10 @@
 import { Route, Redirect } from 'react-router-dom';
 import Setup from '../pages/Setup';
-import Dashboard from '../pages/Dashboard';
 import Login from '../pages/Login';
+import Dashboard from '../pages/Dashboard';
+
+import ProtectedRoute from '../components/ProtectedRoute';
+import AdminLayout from '../layouts/AdminLayout';
 
 export default function AppRouter() {
   return (
@@ -12,7 +15,18 @@ export default function AppRouter() {
 
       <Route path="/setup" component={Setup} />
       <Route path="/login" component={Login} />
-      <Route path="/dashboard" component={Dashboard} />
+
+      {/* ADMIN AREA */}
+      <ProtectedRoute>
+        <AdminLayout>
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/users" component={() => <div>Users</div>} />
+          <Route path="/piggeries" component={() => <div>Piggeries</div>} />
+          <Route path="/devices" component={() => <div>Devices</div>} />
+          <Route path="/sensor-data" component={() => <div>Sensor Data</div>} />
+          <Route path="/notifications" component={() => <div>Notifications</div>} />
+        </AdminLayout>
+      </ProtectedRoute>
     </>
   );
 }
