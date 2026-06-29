1e42569b71790de6273dc88cdf868e49a742a0ce
Arky <20231736@nbsc.edu.ph>
2026-06-29 08:59:36 +0800

    vercel preparation

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-/// <reference types="vitest" />
-
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
   },
+  server: {
+    port: 3000
+  }
 })
