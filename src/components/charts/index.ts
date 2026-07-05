017815ecd7af496e4e07befe041033ed77533875
Arky <20231736@nbsc.edu.ph>
2026-07-05 16:12:54 +0800

    created charts index.ts to import all component

---
+++ src/components/charts/index.ts
@@ -0,0 +1,7 @@
+export { default as AmmoniaTrendChart } from './AmmoniaTrendChart';
+export { default as AlertSeverityChart } from './AlertSeverityChart';
+export { default as AlertTrendChart } from './AlertTrendChart';
+export { default as DeviceStatusChart } from './DeviceStatusChart';
+export { default as TopAlertingDevicesChart } from './TopAlertingDevicesChart';
+export { default as ClientsPiggeriesChart } from './ClientsPiggeriesChart';
+export { default as StatsCard } from './StatsCard';
