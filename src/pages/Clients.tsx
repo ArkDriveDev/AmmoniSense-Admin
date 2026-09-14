9339df2ece554f6f8569fbb6c7cf69a0cb6bb315
Arky <20231736@nbsc.edu.ph>
2026-09-14 10:21:15 +0800

    refactor(clients): remove Clients page (step 10)

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -1,38 +1,3 @@
 import {
   IonPage,
   IonContent,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonList,
-  IonItem,
-  IonLabel,
-  IonButton,
-  IonModal,
-  IonInput,
-  IonButtons,
-  IonIcon,
-  IonToast,
-  IonSearchbar,
-  IonBadge
-} from '@ionic/react';
-
-import { useEffect, useState } from 'react';
-import { supabase } from '../services/supabase';
-import { 
-  businessOutline,
-  trashOutline,
-  createOutline,
-  addOutline,
-  personOutline
-} from 'ionicons/icons';
-
-import DeleteAlert from '../components/DeleteAlert';
-import ConfirmAlert from '../components/ConfirmAlert';
-import EmptyState from '../components/EmptyState';
-import LoadingSpinner from '../components/LoadingSpinner';
-import AssignLivestockModal from '../components/AssignLivestockModal';
-import { useClients } from '../hooks/useClients';
-
-export default function Clients() {
-  const { clients, loading, fetchClients } = useClients();
