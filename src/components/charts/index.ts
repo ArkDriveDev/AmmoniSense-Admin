f899dc9eaa243611fd8e39300ffac8b8203a16c1
Arky <20231736@nbsc.edu.ph>
2026-09-14 10:21:10 +0800

    refactor(charts): update chart index exports

--- src/components/charts/index.ts
+++ src/components/charts/index.ts
@@ -4,4 +4,3 @@ export { default as AlertSeverityChart } from './AlertSeverityChart';
 export { default as AlertTrendChart } from './AlertTrendChart';
 export { default as DeviceStatusChart } from './DeviceStatusChart';
 export { default as TopAlertingDevicesChart } from './TopAlertingDevicesChart';
-export { default as ClientsLivestockChart } from './ClientsLivestockChart';  // Renamed
