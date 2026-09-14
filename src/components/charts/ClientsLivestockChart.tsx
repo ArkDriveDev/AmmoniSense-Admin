a6a74a0ed63568ece5793ec20631e473fae0de9b
JethroBalaga <balagajet@gmail.com>
2026-09-14 10:21:12 +0800

    refactor(charts): remove ClientsLivestockChart (step 1)

--- src/components/charts/ClientsLivestockChart.tsx
+++ src/components/charts/ClientsLivestockChart.tsx
@@ -33,38 +33,3 @@ interface ClientsLivestockChartProps {
 
 export default function ClientsLivestockChart({ data }: ClientsLivestockChartProps) {
   const options = {
-    indexAxis: 'y' as const,
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-      legend: {
-        display: false,
-      },
-      title: {
-        display: true,
-        text: 'Clients with Most Livestock',
-        font: {
-          size: 16,
-          weight: 'bold' as const,
-        },
-      },
-    },
-    scales: {
-      x: {
-        beginAtZero: true,
-        title: {
-          display: true,
-          text: 'Number of Livestock',
-        },
-      },
-      y: {
-        title: {
-          display: true,
-          text: 'Client',
-        },
-      },
-    },
-  };
-
-  return <Bar options={options} data={data} />;
-}
